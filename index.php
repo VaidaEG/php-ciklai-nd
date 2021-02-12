e8db9390a56c2f28122515223decd4c9bb994fbf
VaidaEG <grausliene.vaida@gmail.com>
2021-02-12 18:14:09 +0200

    Loops 7-10 tasks

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ echo '<br><br>';
 $newStrings = ceil(mb_strlen($string) / 50);
 for ($i = 0; $i < $newStrings; $i++) {
     echo substr($string, ($i * 50), 50);
+    echo '<br>';
 }
 ?>
 <h1>------------------------------------2.--------------------------------------</h1>
@@ -86,7 +87,95 @@ do {
 } while (!str_contains($rez, 'HHH'));
 ?>
 <h1>------------------------------------7.--------------------------------------</h1>
+<?php
+$scoreKazysSum = 0;
+$scorePetrasSum = 0;
+while ($scoreKazysSum <= 222 && $scorePetrasSum <= 222) {
+    $scoreKazys = rand(5, 25);
+    $scoreKazysSum += $scoreKazys;
+    $scorePetras = rand(10, 20);
+    $scorePetrasSum += $scorePetras;
+}
+$winner = ($scoreKazysSum > $scorePetrasSum) ? 'Kazys' : 'Petras';
+echo "Kazys has $scoreKazysSum scores, Petras has $scorePetrasSum scores. The winner is $winner.";
+?>
 <h1>------------------------------------8.--------------------------------------</h1>
+<div style="margin-left: 300px;">
+<?php
+ for($i=0;$i<=10;$i++){
+    for ($d=11-$i; $d > 0; $d--)  {
+ 
+        echo "&nbsp;&nbsp;";
+    }
+    for($j=1;$j<=$i;$j++){
+ 
+        echo "&nbsp;"."<span style='color:rgb(". rand(0,255) . ', ' . rand(0,255) . ', ' . rand(0,255) . ")'>*</span>"."&nbsp;";
+    }
+    echo "<br>";
+}
+ for($i=11;$i>=1;$i--){
+    for ($d=1; $d <= 11-$i; $d++)  {
+        echo "&nbsp;&nbsp;";
+    }
+    for($j=$i;$j>=1;$j--){
+        echo "&nbsp;"."<span style='color:rgb(". rand(0,255) . ', ' . rand(0,255) . ', ' . rand(0,255) . ")'>*</span>"."&nbsp;";
+    }
+    echo "<br>";
+}
+?>
+</div>
 <h1>------------------------------------9.--------------------------------------</h1>
+<?php
+$startTime = microtime(true);
+for ($i = 0; $i < 1000000; $i++) {
+    $c = "10 bezdzioniu \n suvalge 20 bananu.";
+}
+$endTime = microtime(true);
+$result = $endTime - $startTime;
+echo "\"10 bezdzioniu \n suvalge 20 bananu.\" time is $result ms.";
+echo '<br>';
+$startTime = microtime(true);
+for ($i = 0; $i < 1000000; $i++) {
+    $c = '10 bezdzioniu \n suvalge 20 bananu.';
+}
+$endTime = microtime(true);
+$result = $endTime - $startTime;
+echo "'10 bezdzioniu \n suvalge 20 bananu.' time is $result ms.";
+?>
 <h1>------------------------------------10.--------------------------------------</h1>
+<h2>a)</h2>
+<?php
+$nailQuantity = 5;
+$nailLength = 85;
+$hitTimes = 0;
+$hitLength = 0;
+while ($hitLength <= ($nailQuantity * $nailLength)) {
+    $hit = rand(5, 20);
+    $hitLength += $hit;
+    $hitTimes++;
+    echo "$hit" . ' mm ';
+}
+echo '<br><br>';
+echo "We need $hitTimes hits (5 - 20 mm) for $nailQuantity nails."
+?>
+<h2>b)</h2>
+<?php
+$nailQuantity = 5;
+$nailLength = 85;
+$hitTimes = 0;
+$hitLength = 0;
+while ($hitLength <= ($nailQuantity * $nailLength)) {
+    $hit = rand(20, 30);
+    $posibility = rand(1, 2);
+    if ($posibility === 1) {
+        $hitLength += $hit;
+        $hitTimes++;
+    } else {
+        $hitTimes++;
+        echo "$hit" . ' mm ';
+    }
+}
+echo '<br><br>';
+echo "We need $hitTimes hits (20 - 30 mm ) for $nailQuantity nails."
+?>
 <h1>------------------------------------11.--------------------------------------</h1>
